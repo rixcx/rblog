7938bd56bc8d5656ae1874fa4b4de23724bc098d
rixcx <adts0415.ovo@gmail.com>
2026-02-11 23:10:42 +0900

    Refactor layout and styling

---
+++ app/pages/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page of your Next.js application.</p>
+      <p>
+        <a href="/">Go to Home Page</a>
+      </p>
+    </div>
+  );
+}
