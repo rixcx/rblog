16658d34f6119f86b36514d3c3886fb540f949e4
rixcx <adts0415.ovo@gmail.com>
2026-02-22 22:40:57 +0900

    Refactor article page design

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -7,11 +7,19 @@ export default async function ArticlePage({ params }: { params: Promise<{ slug: 
 
   return (
     <article className="">
-      <header className="mb-20">
-        <h1 className="text-5xl font-bold mb-2">{article.title}</h1>
-        <data className="inline-block font-playfair text-md transform scale-y-80">{formatDate(article.date)}</data>
-      </header>
-      <div>{article.content}</div>
+      <div className="border-deco-x">
+        <header className="border-deco-b">
+          <div className="py-10 px-6.5">
+            <h1 className="text-5xl font-bold mb-2">{article.title}</h1>
+            <data className="inline-block font-playfair text-md transform scale-y-80">{formatDate(article.date)}</data>
+          </div>
+          <span className="border-deco-joint-r"></span>
+          <span className="border-deco-joint-l"></span>
+        </header>
+        <div className="pt-10 px-6.5 pb-14">
+          {article.content}
+        </div>
+      </div>
     </article>
   );
 }
